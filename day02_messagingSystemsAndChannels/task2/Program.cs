1bb465aabc07a7946b188178ecf12547fac190d0
luka2477 <luke.da.duke@hotmail.com>
2023-02-05 21:11:37 +0100

    init, task 2-3

---
+++ day02_messagingSystemsAndChannels/task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace task2;
+class Program
+{
+	static void Main(string[] args)
+	{
+		const string HostName = "localhost";
+		const int Port = 5672;
+		const string UserName = "guest";
+		const string Password = "pass123!";
+		const string QueueName = "TestQueue";
+
+        ConnectionFactory factory = new ()
+        {
+			HostName = HostName,
+			Port = Port,
+			UserName = UserName,
+			Password = Password
+		};
+		IConnection connection = factory.CreateConnection();
+		IModel channel = connection.CreateModel();
+
+		channel.QueueDeclare(
+			queue: QueueName,
+			durable: false,
+			exclusive: false,
+			autoDelete: false,
+			arguments: null
+		);
+
+		const string message = "Hello World!";
+		byte[] body = Encoding.UTF8.GetBytes(message);
+
+		channel.BasicPublish(
+			exchange: string.Empty,
+			routingKey: QueueName,
+			basicProperties: null,
+			body: body
+		);
+
+		Console.WriteLine($" [x] Sent {message}");
+
+		Console.WriteLine(" Press [enter] to exit.");
+		Console.ReadLine();
+	}
+}
+
---
+++ day02_messagingSystemsAndChannels/task3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace task3;
+class Program
+{
+	static void Main(string[] args)
+	{
+        const string HostName = "localhost";
+        const int Port = 5672;
+        const string UserName = "guest";
+        const string Password = "pass123!";
+        const string QueueName = "TestQueue";
+
+        ConnectionFactory factory = new ()
+		{
+			HostName = HostName,
+			Port = Port,
+			UserName = UserName,
+			Password = Password
+		};
+		IConnection connection = factory.CreateConnection();
+		IModel channel = connection.CreateModel();
+
+		channel.QueueDeclare(
+			queue: QueueName,
+			durable: false,
+			exclusive: false,
+			autoDelete: false,
+			arguments: null
+		);
+
+		Console.WriteLine(" [*] Waiting for messages.");
+
+        EventingBasicConsumer consumer = new (channel);
+		consumer.Received += (model, ea) =>
+		{
+			byte[] body = ea.Body.ToArray();
+			string message = Encoding.UTF8.GetString(body);
+			Console.WriteLine($" [x] Received {message}");
+		};
+		channel.BasicConsume(
+			queue: QueueName,
+			autoAck: true,
+			consumer: consumer
+		);
+
+		Console.WriteLine(" Press [enter] to exit.");
+		Console.ReadLine();
+	}
+}
+
