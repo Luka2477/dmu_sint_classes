f48ee91a6d417b11f0f0d12db3f4ee9efd748568
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-02-14 15:30:18 +0100

    feature: init day05, done task2

---
+++ day05_message_construction/task2/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+
+namespace task2;
+class Program
+{
+	public static void Main(string[] args)
+	{
+		RPCClient client = new((rkey, arrival) =>
+		{
+			Console.WriteLine("Callback called...");
+		})
+		{
+			UserName = "guest",
+			Password = "pass123!",
+			Prefix = "CLIENT",
+			RequestQueueName = "rpc_request",
+			ReplyQueueName = "rpc_reply",
+		};
+		client.Connect();
+		client.CreateQueue();
+		client.StartListening();
+
+		RPCServer server = new(new List<Messages.Arrival> {
+			new Messages.Arrival("SAS", "SAS1234", "B12", new DateTime(2022, 02, 14, 12, 30, 00)),
+			new Messages.Arrival("KLM", "KLM1234", "B13", new DateTime(2022, 02, 14, 13, 30, 00)),
+			new Messages.Arrival("SW" , "SW1234" , "B14", new DateTime(2022, 02, 14, 14, 30, 00)),
+		})
+		{
+			UserName = "guest",
+			Password = "pass123!",
+			Prefix = "SERVER",
+			RequestQueueName = "rpc_request",
+		};
+		server.Connect();
+		server.StartListening();
+
+		// -------------------------------------------------------------------------
+
+		client.SendMessage("SAS1234");
+		Thread.Sleep(250);
+		client.SendMessage("KLM1234");
+		Thread.Sleep(250);
+		client.SendMessage("SW0000");
+
+		Console.ReadLine();
+	}
+}
+
---
+++ day05_message_construction/task2/RPCServer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Channels;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace task2
+{
+	public class RPCServer
+	{
+		public string? HostName { get; set; } = "localhost";
+		public int Port { get; set; } = 5672;
+		public string? UserName { private get; set; }
+		public string? Password { private get; set; }
+		public string? RequestQueueName { get; set; }
+		public string? Prefix { get; set; }
+
+		private ConnectionFactory? _factory;
+		private IConnection? _connection;
+		private IModel? _channel;
+
+		private string? _consumerTag;
+		private List<Messages.Arrival> _arrivals;
+
+		public RPCServer(List<Messages.Arrival> arrivals)
+		{
+			_arrivals = arrivals ?? throw new ArgumentNullException(nameof(arrivals));
+		}
+
+		public void Connect()
+		{
+			_factory = new()
+			{
+				HostName = HostName,
+				Port = Port,
+				UserName = UserName,
+				Password = Password
+			};
+			_connection = _factory.CreateConnection();
+			_channel = _connection.CreateModel();
+
+			_channel.QueueDeclare(
+				queue: RequestQueueName,
+				durable: false,
+				exclusive: false,
+				autoDelete: false,
+				arguments: null
+			);
+
+			Console.WriteLine($" [{Prefix}] Connected to RabbitMQ on '{HostName}':'{Port}'");
+		}
+
+		public void StartListening()
+		{
+			Console.WriteLine($" [{Prefix}] Started listening for messages...");
+
+			EventingBasicConsumer consumer = new(_channel);
+			consumer.Received += (model, ea) =>
+			{
+				IBasicProperties props = ea.BasicProperties;
+				IBasicProperties replyProps = _channel.CreateBasicProperties();
+				replyProps.CorrelationId = props.CorrelationId;
+
+				byte[] body = ea.Body.ToArray();
+				string message = Encoding.UTF8.GetString(body);
+				string routingKey = ea.RoutingKey;
+
+				Console.WriteLine($" [{Prefix}] Received '{routingKey}':'{message}'");
+
+				string res = "INVALID";
+				foreach (Messages.Arrival arrival in _arrivals)
+				{
+					if (arrival.FlightID.Equals(message))
+					{
+						res = JsonSerializer.Serialize(arrival);
+					}
+				}
+
+				byte[] resBytes = Encoding.UTF8.GetBytes(res);
+				_channel.BasicPublish(exchange: string.Empty,
+														 routingKey: props.ReplyTo,
+														 basicProperties: replyProps,
+														 body: resBytes);
+				_channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+
+				Console.WriteLine($" [{Prefix}] Sent '{props.ReplyTo}':'{res}'");
+			};
+			_consumerTag = _channel.BasicConsume(
+				queue: RequestQueueName,
+				autoAck: false,
+				consumer: consumer
+			);
+		}
+
+		public void StopListening()
+		{
+			_channel.BasicCancel(
+				consumerTag: _consumerTag
+			);
+
+			Console.WriteLine($" [{Prefix}] Stopped listening for messages...");
+		}
+	}
+}
+
