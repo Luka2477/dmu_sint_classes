b78b0a8ab254982c2340c5acaf89c863a61cae29
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-02-20 10:02:49 +0100

    feature: done task 1

--- day07_message_construction/task1/Program.cs
+++ day07_message_construction/task1/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace task1;
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace task1;
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+	static void Main(string[] args)
+	{
+		ConnectionFactory factory = new()
+		{
+			UserName = "guest",
+			Password = "pass123!"
+		};
+		IConnection connection = factory.CreateConnection();
+		IModel channel = connection.CreateModel();
+
+		string queueName = channel.QueueDeclare(
+			queue: "day07_task1",
+			durable: true,
+			exclusive: false,
+			autoDelete: false,
+			arguments: null
+		).QueueName;
+
+		IBasicProperties properties = channel.CreateBasicProperties();
+		properties.Expiration = "10000";
+
+		byte[] message = Encoding.UTF8.GetBytes("Hello mother");
+		channel.BasicPublish(
+			exchange: string.Empty,
+			routingKey: queueName,
+			basicProperties: properties,
+			body: message
+		);
+	}
 }
 
