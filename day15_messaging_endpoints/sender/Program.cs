e2873640bf04bbcb274bd6db04540859fa1442b8
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-03-21 10:12:20 +0100

    feature: task1 init

---
+++ day15_messaging_endpoints/sender/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+using System.Xml.Linq;
+using RabbitMQ.Client;
+
+namespace sender;
+class Program
+{
+	private const string PathToXml = "/Users/lukasknudsen/Documents/dmu/sint/dmu_sint_classes/day09_mini_proj/sender/Assets/Data.xml";
+
+	private static void Main(string[] args)
+	{
+		// Connect to RabbitMQ
+		ConnectionFactory factory = new()
+		{
+			UserName = "guest",
+			Password = "pass123!",
+		};
+		IConnection connection = factory.CreateConnection();
+		IModel channel = connection.CreateModel();
+
+		Console.WriteLine("Connected to RabbitMQ");
+
+		// Create queue
+		string queueName = channel.QueueDeclare(
+			queue: "day09_checkin",
+			durable: false,
+			exclusive: false,
+			autoDelete: false,
+			arguments: null
+		).QueueName;
+
+		Console.WriteLine($"Created queue '{queueName}'");
+		Console.WriteLine();
+
+		// Main loop
+		while (true)
+		{
+			Console.WriteLine("Type 'send' to send message...");
+			Console.WriteLine("Leave empty to exit...");
+			string input = Console.ReadLine();
+
+			switch (input)
+			{
+				case "send":
+					Console.WriteLine();
+					SendMessage(channel, queueName);
+					break;
+				case "":
+					return;
+				default:
+					Console.WriteLine();
+					Console.WriteLine("Unknown command...");
+					Console.WriteLine();
+					break;
+			}
+		}
+	}
+	
+
+	private static void SendMessage(IModel channel, string queueName)
+	{
+		// Load XML from file
+		XElement xml = XElement.Load(PathToXml);
+
+		Console.WriteLine("Loaded XML file");
+		
+		// Generate CorrelationID
+		IBasicProperties props = channel.CreateBasicProperties();
+		props.CorrelationId = Guid.NewGuid().ToString();
+
+		// Convert XML to byte array
+		byte[] bytes = Encoding.UTF8.GetBytes(xml.ToString());
+
+		Console.WriteLine("Converted XML to byte array");
+		Console.WriteLine();
+
+		// Publish message to queue
+		channel.BasicPublish(
+			exchange: string.Empty,
+			routingKey: queueName,
+			basicProperties: props,
+			body: bytes
+		);
+
+		Console.WriteLine($"Sent message to '{queueName}' with body:");
+		Console.WriteLine(xml.ToString());
+		Console.WriteLine();
+	}
+}
+
---
+++ day15_messaging_endpoints/splitter/Program.cs
@@ -0,0 +1,131 @@
+﻿using System.Text;
+using System.Xml.Linq;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace splitter;
+class Program
+{
+	static void Main(string[] args)
+	{
+		// Connect to RabbitMQ
+		ConnectionFactory factory = new()
+		{
+			UserName = "guest",
+			Password = "pass123!",
+		};
+		IConnection connection = factory.CreateConnection();
+		IModel channel = connection.CreateModel();
+
+		Console.WriteLine("Connected to RabbitMQ");
+
+		// Create checkin queue
+		string receiveQueueName = channel.QueueDeclare(
+			queue: "day09_checkin",
+			durable: false,
+			exclusive: false,
+			autoDelete: false,
+			arguments: null
+		).QueueName;
+
+		Console.WriteLine($"Created queue '{receiveQueueName}'");
+
+		// Create dummy queues
+		string luggageQueueName = channel.QueueDeclare(
+			queue: "day09_luggage",
+			durable: false,
+			exclusive: false,
+			autoDelete: false,
+			arguments: null
+		).QueueName;
+
+		Console.WriteLine($"Created dummy queue '{luggageQueueName}'");
+
+		string passengerQueueName = channel.QueueDeclare(
+			queue: "day09_passenger",
+			durable: false,
+			exclusive: false,
+			autoDelete: false,
+			arguments: null
+		).QueueName;
+
+		Console.WriteLine($"Created dummy queue '{passengerQueueName}'");
+
+		// Receive messages in checkin queue
+		EventingBasicConsumer consumer = new(channel);
+		consumer.Received += (ch, ea) =>
+		{
+			Console.WriteLine($"Received message from '{ea.RoutingKey}'");
+			Console.WriteLine();
+			
+			// Generate CorrelationID
+			IBasicProperties props = channel.CreateBasicProperties();
+			props.CorrelationId = ea.BasicProperties.CorrelationId;
+
+			// Get XML from byte array
+			XElement xml = XElement.Parse(Encoding.UTF8.GetString(ea.Body.ToArray()));
+
+			// Parse Luggage and Passenger elements
+			IEnumerable<XElement> luggages = xml.Elements("Luggage");
+			IEnumerable<XElement> passengers = xml.Elements("Passenger");
+
+			// Send Luggage data to dummy queue
+			List<XElement> luggagesList = luggages.ToList();
+			foreach (XElement luggage in luggagesList)
+			{
+				// Add data to support sliding window principle
+				int sequence = int.Parse(luggage.Element("Identification")?.Value);
+				
+				XElement data = new XElement("Data");
+				data.Add(new XElement("Total", luggagesList.Count));
+				data.Add(new XElement("Sequence", sequence));
+				luggage.AddFirst(data);
+				
+				byte[] bytes = Encoding.UTF8.GetBytes(luggage.ToString());
+
+				channel.BasicPublish(
+					exchange: string.Empty,
+					routingKey: luggageQueueName,
+					basicProperties: props,
+					body: bytes
+				);
+
+				Console.WriteLine($"Sent message to '{luggageQueueName}' with body:");
+				Console.WriteLine(luggage.ToString());
+				Console.WriteLine();
+			}
+
+			// Send Passenger data to dummy queue
+			foreach (XElement passenger in passengers)
+			{
+				byte[] bytes = Encoding.UTF8.GetBytes(passenger.ToString());
+
+				channel.BasicPublish(
+					exchange: string.Empty,
+					routingKey: passengerQueueName,
+					basicProperties: props,
+					body: bytes
+				);
+
+				Console.WriteLine($"Sent message to '{passengerQueueName}' with body:");
+				Console.WriteLine(passenger.ToString());
+				Console.WriteLine();
+			}
+		};
+
+		// Attach consumer to checkin queue
+		channel.BasicConsume(
+			queue: receiveQueueName,
+			autoAck: true,
+			consumer: consumer
+		);
+
+		Console.WriteLine($"Listening to messages on '{receiveQueueName}'");
+		Console.WriteLine();
+
+		Console.WriteLine("Press 'ENTER' to exit...");
+		Console.WriteLine();
+		Console.Read();
+	}
+}
+
