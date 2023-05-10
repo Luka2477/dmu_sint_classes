d717dca6a2a5845a28ac16a5f17eed1208287beb
Lukas Knudsen <luke.da.duke@hotmail.com>
2023-05-10 22:49:48 +0200

    some stuff

---
+++ eksamensprojekt/code/router/Program.cs
@@ -0,0 +1,136 @@
+﻿using System.Text;
+using System.Xml.Linq;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+
+// Create connection to RabbitMQ
+ConnectionFactory factory = new()
+{
+	UserName = "guest",
+	Password = "pass123!",
+};
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+Console.WriteLine("Connected to RabbitMQ");
+
+
+
+// Create in queue
+string CREADQueueName = channel.QueueDeclare(
+	queue: "exam_c-read-out",
+	durable: false,
+	exclusive: false,
+	autoDelete: false,
+	arguments: null
+).QueueName;
+Console.WriteLine($"Created in queue '{CREADQueueName}'");
+
+
+
+// Create out queues
+string STASQueueName = channel.QueueDeclare(
+	queue: "exam_stas-in",
+	durable: false,
+	exclusive: false,
+	autoDelete: false,
+	arguments: null
+).QueueName;
+Console.WriteLine($"Created out queue '{STASQueueName}'");
+
+string SSQueueName = channel.QueueDeclare(
+	queue: "exam_ss-in",
+	durable: false,
+	exclusive: false,
+	autoDelete: false,
+	arguments: null
+).QueueName;
+Console.WriteLine($"Created out queue '{SSQueueName}'");
+
+string LMSQueueName = channel.QueueDeclare(
+	queue: "exam_lms-in",
+	durable: false,
+	exclusive: false,
+	autoDelete: false,
+	arguments: null
+).QueueName;
+Console.WriteLine($"Created out queue '{LMSQueueName}'");
+
+string PAYSQueueName = channel.QueueDeclare(
+	queue: "exam_pays-in",
+	durable: false,
+	exclusive: false,
+	autoDelete: false,
+	arguments: null
+).QueueName;
+Console.WriteLine($"Created out queue '{PAYSQueueName}'");
+
+string OutQueueName = channel.QueueDeclare(
+	queue: "exam_out",
+	durable: false,
+	exclusive: false,
+	autoDelete: false,
+	arguments: null
+).QueueName;
+Console.WriteLine($"Created out queue '{OutQueueName}'");
+Console.WriteLine();
+
+
+
+// Define our consume which we will later use to subscribe to our in queue ('exam_c-reads-out')
+EventingBasicConsumer consumer = new(channel);
+consumer.Received += (ch, ea) =>
+{
+	Console.WriteLine($"Received message from '{ea.RoutingKey}'");
+	Console.WriteLine();
+
+	// Get XML from byte array
+	XElement root = XElement.Parse(Encoding.UTF8.GetString(ea.Body.ToArray()));
+	
+	// Initialize routingKey to be the out queue ('exam_out')
+	string routingKey = OutQueueName;
+	// If the root elements name is 'AttendanceRegistration' or 'Payment'
+	if (root.Name == "AttendanceRegistration")
+	{
+		// If payload contains 'Course' then it should be sent to LMS ('exam_lms-in')
+		if (root.Element("Course") != null) 
+			routingKey = LMSQueueName;
+		// If payload contains 'Class' then it should be sent to SS ('exam_ss-in')
+		else if (root.Element("Class") != null)
+			routingKey = SSQueueName;
+		// Else it should be sent to STAS ('exam_stas-in')
+		else routingKey = STASQueueName;
+	}
+	// If the root elements name is 'Payment' it should be sent to ('exam_pays-in')
+	else if (root.Name == "Payment")
+		routingKey = PAYSQueueName;
+
+	// Send the payload to the relevant queue via the routingKey
+	channel.BasicPublish(
+		exchange: string.Empty,
+		routingKey: routingKey,
+		body: ea.Body
+	);
+
+	Console.WriteLine($"Sent message to '{routingKey}' with body:");
+	Console.WriteLine(ea.Body.ToString());
+	Console.WriteLine();
+};
+
+
+
+// Subscribe to our in queue ('exam_c-reads-out')
+channel.BasicConsume(
+	queue: CREADQueueName,
+	autoAck: true,
+	consumer: consumer
+);
+
+Console.WriteLine($"Listening to messages on '{CREADQueueName}'");
+Console.WriteLine();
+
+Console.WriteLine("Press 'ENTER' to exit...");
+Console.WriteLine();
+Console.Read();
---
+++ eksamensprojekt/code/simulated_sender/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Text;
+using System.Xml.Linq;
+using RabbitMQ.Client;
+
+
+// Connect to RabbitMQ
+ConnectionFactory factory = new()
+{
+    UserName = "guest",
+    Password = "pass123!",
+};
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+Console.WriteLine("Connected to RabbitMQ");
+
+
+
+// Create out queue
+string queueName = channel.QueueDeclare(
+    queue: "exam_c-read-out",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+).QueueName;
+
+Console.WriteLine($"Created queue '{queueName}'");
+Console.WriteLine();
+
+
+
+// Load XML from file
+XElement root =
+    XElement.Load(
+        "/Users/lukasknudsen/Documents/dmu/sint/dmu_sint_classes/eksamensprojekt/code/simulated_sender/Assets/data.xml");
+Console.WriteLine("Loaded XML file");
+
+
+
+// Loop asking for what message you would like to send
+bool shouldRun = true;
+while (shouldRun)
+{
+    Console.Write("What message would you like to send? [STAS, SS, LMS, PAYS, OUT] ");
+    string input = Console.ReadLine().ToLower();
+
+    switch (input)
+    {
+        case "stas":
+            SendMessage(root.Element("STAS").Element("AttendanceRegistration"));
+            break;
+        case "ss":
+            SendMessage(root.Element("SS").Element("AttendanceRegistration"));
+            break;
+        case "lms":
+            SendMessage(root.Element("LMS").Element("AttendanceRegistration"));
+            break;
+        case "pays":
+            SendMessage(root.Element("PAYS").Element("Payment"));
+            break;
+        case "out":
+            SendMessage(root.Element("Out").Element("Response"));
+            break;
+        case "":
+            shouldRun = false;
+            break;
+        default:
+            Console.WriteLine("Unknown command. Please try again...");
+            Console.WriteLine();
+            break;
+    }
+}
+
+
+
+// Method for sending messages into the out queue
+void SendMessage(XElement message)
+{
+    // Convert XML to byte array
+    byte[] bytes = Encoding.UTF8.GetBytes(message.ToString());
+
+    Console.WriteLine("Converted XML to byte array");
+    Console.WriteLine();
+
+    // Publish message to queue
+    channel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: queueName,
+        body: bytes
+    );
+    
+    Console.WriteLine($"Sent message to '{queueName}' with body:");
+    Console.WriteLine(message.ToString());
+    Console.WriteLine();
+}
